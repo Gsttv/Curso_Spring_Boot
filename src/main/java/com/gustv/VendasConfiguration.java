622e8ebe5712234846e6a5e130d0c7e4d3a6f6d0
Gustavo Morais <gustvmorais@gmail.com>
2023-08-10 15:38:16 -0300

    Class Configuration

--- src/main/java/com/gustv/VendasConfiguration.java
+++ src/main/java/com/gustv/VendasConfiguration.java
@@ -1,7 +1,12 @@
 package com.gustv;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class VendasConfiguration {
+    @Bean(name = "applicationName")
+    public String applicationName(){
+        return "Sistemas de Vendas";
+    }
 }
